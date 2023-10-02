15cdd887055a645b21abb6702b6dc4d008f48d5f
GeorgeKlemperer <george@klemperer.co.uk>
2023-10-02 12:20:57 +0100

    challenge done

--- challenge/read-file.js
+++ challenge/read-file.js
@@ -1,7 +1,17 @@
 const fs = require("node:fs");
 
 function readFilePromise(filePath) {
-  //
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        reject(err); // Reject with the error
+      } else {
+        resolve(data); // Resolve with the file contents
+      }
+    });
+  });
+
+  return promise;
 }
 
 module.exports = { readFilePromise };
--- challenge/wait.js
+++ challenge/wait.js
@@ -1,5 +1,9 @@
 function wait(ms) {
-  //
+  const promise = new Promise((resolve) => {
+    // Fixed syntax for creating a new Promise
+    setTimeout(resolve, ms);
+  });
+  return promise; // You should return the promise so that it can be used elsewhere
 }
 
 module.exports = { wait };
